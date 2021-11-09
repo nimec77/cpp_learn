c67c6dd7b53f220c2dae6e9a8277e1e06f54b25a
comrade77 <nimec77@gmail.com>
2021-11-09 11:55:29 +0700

    Добавил к knapsack_problem_test.cpp тест с youtube

--- test/dp/knapsack_problem_test.cpp
+++ test/dp/knapsack_problem_test.cpp
@@ -9,3 +9,8 @@ TEST(KnapsackProblemTest, SimpleTest) {
     const auto result = KnapsackProblem::CountMax({4, 1, 3}, {4000, 2500, 2000}, 4);
     ASSERT_EQ(result, 4500);
 }
+
+TEST(KnapsackProblemTest, YoutubeTest) {
+    const auto result = KnapsackProblem::CountMax({6, 4, 3, 2 , 5}, {5, 3, 1, 3, 6}, 15);
+    ASSERT_EQ(result, 14);
+}
